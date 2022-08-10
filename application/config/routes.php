8ab58ba7818dfd0ee670720fb3a3d97b4ea11f1f
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-10 18:29:37 +0200

    added appointments routes

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['appointments/(:any)'] = 'appointments/view/$1';
+$route['appointments'] = 'appointments/index';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
