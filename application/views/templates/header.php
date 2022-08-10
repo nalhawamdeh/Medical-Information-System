113e9dac726977c38eedc1b84084ee712747d587
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-10 18:30:49 +0200

    added local style sheet and appointments option in navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,19 +1,21 @@
 <html>
     <head>
-        <title> Medical Information System </title>
+        <title> Medical Info System </title>
         <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/styles.css">
     </head>
     <body>
     <nav class="navbar navbar-inverse">
 		<div class="container">
 				<div class="navbar-header">
-					<a class="navbar-brand" href="<?php echo base_url(); ?>">Medical Information System</a>
+					<a class="navbar-brand" href="<?php echo base_url(); ?>">Medical Info System</a>
 				</div>
 			<div id="navbar">
 				<ul class="nav navbar-nav">
 					<li><a href="<?php echo base_url(); ?>">Home</a></li>
-					<li><a href="<?php echo base_url(); ?>/about">About</a></li>
-					<li><a href="<?php echo base_url(); ?>/contact">Contact Us</a></li>
+					<li><a href="<?php echo base_url(); ?>about">About</a></li>
+					<li><a href="<?php echo base_url(); ?>contact">Contact Us</a></li>
+                    <li><a href="<?php echo base_url(); ?>appointments">Appointments</a></li>
                 </ul>
             </div>
         </div>
