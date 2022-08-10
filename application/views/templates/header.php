839521969a64a76e8cfd217ff35d5d59d640211a
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-10 13:53:12 +0200

    added bootstrap to header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,6 @@
 <html>
     <head>
         <title> Medical Information System </title>
+        <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
     </head>
     <body>
