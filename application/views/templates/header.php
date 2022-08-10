e37d11ee569584d8a85095253beca3320c87967d
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-10 14:46:03 +0200

    added navbar with pages and container div

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,3 +4,19 @@
         <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
     </head>
     <body>
+    <nav class="navbar navbar-inverse">
+		<div class="container">
+				<div class="navbar-header">
+					<a class="navbar-brand" href="<?php echo base_url(); ?>">Medical Information System</a>
+				</div>
+			<div id="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="<?php echo base_url(); ?>">Home</a></li>
+					<li><a href="<?php echo base_url(); ?>/about">About</a></li>
+					<li><a href="<?php echo base_url(); ?>/contact">Contact Us</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
