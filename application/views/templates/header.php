3925b1065024e780aff4b949e5252ea22227335a
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-11 12:24:30 +0200

    added date dropdown styles ref and js script tag

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,27 @@
         <title> Medical Info System </title>
         <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/styles.css">
+        <<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     </head>
+
+    <script>
+    $(document).ready(function(){
+      var date_input=$('input[name="date"]'); //our date input has the name "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      var options={
+        format: 'yyyy/mm/dd',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
+    })
+    </script>
+    
+    
+
     <body>
     <nav class="navbar navbar-inverse">
 		<div class="container">
