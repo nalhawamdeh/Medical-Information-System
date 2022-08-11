7a8cb7f594a408d3f70f826c059ffc30f9657088
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-11 19:00:27 +0200

    created choose profile page for registration

---
+++ application/views/pages/registerprofiles.php
@@ -0,0 +1,16 @@
+<h2 class="text-primary page-title" style="text-align:center;"> Are you a Doctor or a Patient ? </h2>
+
+<div class="button-container">
+    <button class="profile-button">
+    	<a class="profile-a" href="<?php echo base_url();?>doctors/register">Doctor
+    	</a>
+	</button>
+</div>
+
+<div class="button-container">
+    <button class="profile-button">
+    	<a class="profile-a" href="<?php echo base_url();?>patients/register">Patient
+    	</a>
+	</button>
+</div>
+
