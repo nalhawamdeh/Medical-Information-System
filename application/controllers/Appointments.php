61a7fee146e0ae21991acebc6a11012a771aa5ac
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-11 12:26:48 +0200

    added book function

--- application/controllers/Appointments.php
+++ application/controllers/Appointments.php
@@ -27,4 +27,26 @@
             $this -> load -> view('templates/footer');
 
         }
+
+        public function book() {
+            $data['title'] = 'Book an Appointment with your Doctor';
+
+            $this -> form_validation -> set_rules('date','Date', 'required');
+            $this -> form_validation -> set_rules('time','Time', 'required');
+            $this -> form_validation -> set_rules('patient_id','Patient ID', 'required');
+            $this -> form_validation -> set_rules('patient_surname','Patient Surname', 'required');
+            $this -> form_validation -> set_rules('patient_email','Patient Email', 'required');
+            $this -> form_validation -> set_rules('doctor_id','Doctor ID', 'required');
+            $this -> form_validation -> set_rules('doctor_surname','Doctor Surname', 'required');
+            $this -> form_validation -> set_rules('doctor_email','Doctor Email', 'required');
+
+            if($this-> form_validation-> run() === FALSE) {
+                $this -> load -> view('templates/header');
+                $this -> load -> view('appointments/book', $data);
+                $this -> load -> view('templates/footer');
+            } else {
+                $this -> appointment_model -> book();
+                redirect('appointments');
+            }
+        }
     }
