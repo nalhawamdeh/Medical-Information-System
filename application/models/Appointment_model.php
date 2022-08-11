3f4acf7e09625a59b6228922870604619a244798
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-11 13:55:09 +0200

    added update and delete functions

--- application/models/Appointment_model.php
+++ application/models/Appointment_model.php
@@ -38,4 +38,31 @@
             #insert into database
             return $this -> db -> insert('appointments', $data);
         }
+
+        public function delete($appointment_id) {
+            $this -> db -> where('appointment_id', $appointment_id);
+            $this -> db -> delete('appointments');
+            return true;
+        }
+
+        public function update(){
+             #create slug before entering into database
+             $slug = url_title($this->input->post('patient_id'));
+
+            #data coming from form
+            $data = array(
+                'slug' => $slug,
+                'date' => $this->input->post('date'),
+                'time' => $this->input->post('time'),
+                'patient_id' => $this->input->post('patient_id'),
+                'patient_surname' => $this->input->post('patient_surname'),
+                'patient_email' => $this->input->post('patient_email'),
+                'doctor_id' => $this->input->post('doctor_id'),
+                'doctor_surname' => $this->input->post('doctor_surname'),
+                'doctor_email' => $this->input->post('doctor_email')
+            );
+
+            $this -> db -> where('appointment_id', $this -> input -> post('appointment_id'));
+            return $this -> db -> update('appointments', $data);
+        }
     }
