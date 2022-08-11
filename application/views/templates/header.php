e1f97f750747056a5dbd52a4a8a94486826c8b97
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-11 13:56:10 +0200

    added book appointment in navbar

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,7 +3,7 @@
         <title> Medical Info System </title>
         <link rel="stylesheet" href="https://bootswatch.com/3/yeti/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/styles.css">
-        <<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
     </head>
@@ -36,9 +36,12 @@
 					<li><a href="<?php echo base_url(); ?>about">About</a></li>
 					<li><a href="<?php echo base_url(); ?>contact">Contact Us</a></li>
                     <li><a href="<?php echo base_url(); ?>appointments">Appointments</a></li>
-                </ul>
-            </div>
-        </div>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="<?php echo base_url();?>appointments/book">Book Appointment</a></li>
+        </ul>
+      </div>
+    </div>
     </nav>
 
     <div class="container">
