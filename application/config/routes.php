35ae3a1fa2237f5ce6286e1b8979a00b68c96d29
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-11 13:57:34 +0200

    added update route

--- application/config/routes.php
+++ application/config/routes.php
@@ -50,6 +50,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['appointments/book'] = 'appointments/book';
+$route['appointments/update'] = 'appointments/update';
 $route['appointments/(:any)'] = 'appointments/view/$1';
 $route['appointments'] = 'appointments/index';
 
