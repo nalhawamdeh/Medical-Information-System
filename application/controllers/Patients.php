2b2c8fec434976924ebbdf19e4b71effabee2c1b
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-12 14:26:10 +0200

    implemented login and logout functions

--- application/controllers/Patients.php
+++ application/controllers/Patients.php
@@ -35,4 +35,57 @@
                 return false;
             }
         }
+
+
+        public function login() {
+            $data['title'] = 'Login';
+
+            $this -> form_validation ->set_rules('patient_email', 'Patient Email', 'required');
+            $this -> form_validation ->set_rules('password', 'Password', 'required');
+           
+
+            if($this -> form_validation -> run() === FALSE){
+                $this -> load -> view('templates/header');
+                $this -> load -> view('patients/login', $data);
+                $this -> load -> view('templates/footer');
+            } else {        
+                //Get email
+                $email = $this -> input -> post('patient_email');
+                $password = md5($this-> input -> post('password'));
+                
+                //Login user
+                $p_id = $this -> patient_model -> login($email, $password);
+
+                if($p_id){
+                    //create the session and alert message
+                    $patient_data = array(
+                        'p_id' => $p_id,
+                        'email' => $email,
+                        'logged_in' => true
+                    );
+
+                    $this -> session -> set_userdata($patient_data);
+                    
+                    $this -> session -> set_flashdata('patient_loggedin','You have logged in successfully.');
+                    redirect('appointments');
+
+                } else {
+                    $this -> session -> set_flashdata('patient_notloggedin','Login failed.');
+                    redirect('patients/login');
+                }
+                
+            }
+
+        }
+
+        public function logout() {
+            //unset data
+            $this -> session -> unset_userdata('logged_in');
+            $this -> session -> unset_userdata('p_id');
+            $this -> session -> unset_userdata('email');
+
+            $this -> session -> set_flashdata('patient_loggedout','You have logged out successfully.');
+            redirect('patients/login');
+        }
+
     }
