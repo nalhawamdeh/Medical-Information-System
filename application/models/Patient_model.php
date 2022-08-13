3f8cb3f87c159c4319c666eb1cc0c8027bc9b325
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-13 17:55:03 +0200

    added view update and edit profile functions

--- application/models/Patient_model.php
+++ application/models/Patient_model.php
@@ -37,4 +37,31 @@
                 return false;
             }
         }
+
+        public function get_patient($patient_email) {
+            $query = $this -> db -> get_where('patient', array('patient_email' => $patient_email)); 
+            return $query -> row_array();
+
+        }
+
+        public function edit() {
+            $data = array(
+                'patient_forename' => $this -> input -> post('patient_forename'),
+                'patient_surname' => $this->input->post('patient_surname'),
+                'patient_email' => $this-> input -> post('patient_email'),
+                'password' => $this -> input -> post('password')
+            );
+
+            $patient_data = array(
+                'p_id' => $this-> input -> post('patient_id'),
+                'email' => $this-> input -> post('patient_email'),
+                'logged_in' => true,
+                'profile' => "patient"
+            );
+
+            $this -> session -> set_userdata($patient_data);
+            $this -> db -> where('patient_id', $this -> input -> post('patient_id'));
+            return $this -> db -> update('patient', $data);
+
+        }
     }
