1c5fcad042a2e107956c380172e62d70f4381d3c
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-13 18:37:17 +0200

    added get doctor and edit functions

--- application/models/Doctor_model.php
+++ application/models/Doctor_model.php
@@ -39,4 +39,32 @@
                 return false;
             }
         }
+
+        public function get_doctor($doctor_email) {
+            $query = $this -> db -> get_where('doctor', array('doctor_email' => $doctor_email)); 
+            return $query -> row_array();
+
+        }
+
+        public function edit() {
+            $data = array(
+                'doctor_forename' => $this -> input -> post('doctor_forename'),
+                'doctor_surname' => $this->input->post('doctor_surname'),
+                'doctor_email' => $this-> input -> post('doctor_email'),
+                'org_id' => $this -> input -> post('org_id'),
+                'med_speciality' => $this -> input -> post('med_speciality'),
+            );
+
+            $doctor_data = array(
+                'd_id' => $this-> input -> post('doctor_id'),
+                'email' => $this-> input -> post('doctor_email'),
+                'logged_in' => true,
+                'profile' => "doctor"
+            );
+
+            $this -> session -> set_userdata($doctor_data);
+            $this -> db -> where('doctor_id', $this -> input -> post('doctor_id'));
+            return $this -> db -> update('doctor', $data);
+
+        }
     }
