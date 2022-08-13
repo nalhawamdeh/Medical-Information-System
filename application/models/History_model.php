24babad764ccc764072d3eebe492cb9a16465fd4
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-13 21:23:58 +0200

    implemented get histories

---
+++ application/models/History_model.php
@@ -0,0 +1,17 @@
+<?php
+    class History_model extends CI_Model {
+        public function __construct() {
+            $this -> load -> database();
+        }
+
+        public function get_histories($slug = FALSE) {
+            if($slug === FALSE) {
+                $query = $this -> db -> get('history');
+                return $query -> result_array();
+            }
+
+            $query = $this -> db -> get_where('history', array('slug' => $slug));
+
+            return $query -> row_array();
+        }
+    }
