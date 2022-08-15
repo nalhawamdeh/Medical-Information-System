19e16004754cce0f8d5d5a0fd7cfd7c94b061aa5
nalhawamdeh <nadeenhawamdeh85@gmail.com>
2022-08-15 12:28:58 +0200

    fixed homepage content

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,25 +1,14 @@
 <h2 class="text-primary page-title"> <?= $title?> </h2>
 
 <p class="p-pages">
-Since the beginning of 2020, 215 countries and territories around the globe have reported many cases of the coronavirus COVID-19. 
-Since this virus is highly contagious, many different demographics are at risk of testing positive if they do not abide by basic 
-rules and procedures to reduce the risk of infection. One of the most emphasized rules is social distancing, which is 
-maintaining a greater than usual physical distance from other people. 
-This ‘obstacle’ of social distancing is occurring all over the world as societies are unable to function as they used to. 
-Several organizations such as educational facilities and businesses are forced to continue their functions online, minimizing contact between their members. 
-One specific area that might be very difficult to manage and deliver online is the healthcare system, as a huge part of it is in person appointments between doctors and patients.
-
+Welcome to the Medical Information System!
 <br><br>
-
-For some specific demographics, such as the elderly, pregnant women and people with underlying health 
-conditions, medical care is needed more often whether its routine checkups, emergency calls, updating medication prescriptions 
-and updating a patient’s medical record/history. With the pandemic changing how safe it 
-is to go into crowded, busy places, it can be difficult for people in need of this specific 
-medical attention to obtain it as they are at risk of catching the virus and their chances of developing severe illness if 
-infected is quite  higher than the average person. If a resolution is found to allow such demographics to access 
-their regular healthcare without putting themselves at risk, hospitals which deal with COVID-19 cases will have a 
-lower influx of patients on their premises which lowers the rate of transmission of COIVD-19 and lowers the in-hospital
-workload for tasks that can be easily done online or at a private medical center/organization.
-
+This system was created for my Systems 3 - Information Systems Course at FAMNIT, University of Primorska in Koper, Slovenia.
+<br><br>
+The system allows Patients to register in order to book appointments and view their medical information or upcoming appointments. 
+The system allows Doctors to register under a pre-exisitng organization, book appointments and edit patient medical information which they have previously
+created.
 </p>
+
+<p class="p-pages"> Technologies and Frameworks used: Code Igniter 3, Bootstrap, PHP, Javascript and CSS </p>
 
